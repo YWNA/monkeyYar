f533c7e130634a455ab1df8603fbed17db36d365
chenbo <chenbo@edusoho.com>
2018-05-23 15:24:49 +0800

    fix迁移

--- index.php
+++ index.php
@@ -1,19 +1,2 @@
 <?php
-require 'vendor/autoload.php';
-require './LogService.php';
-use Pimple\Container;
-class API {
-    public function rpc($className, $functionName){
-        $container = new Container();
-        $container[$className] = function () use($className){
-            $stdClass = 'root\\'.$className;
-            return new $stdClass;
-        };
-        $test = $container[$className];
-        $test->$functionName(date('H:i:s',time()) . \root\LogService::class);
-    }
-}
-
-$service = new Yar_Server(new API());
-$service->handle();
-?>
+echo phpinfo();
